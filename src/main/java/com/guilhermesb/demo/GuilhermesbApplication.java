1ad05469f6e8998449c114cbc1e1e068d0c06bfb
Gui5bezerra <guilhermesbezerra1@gmail.com>
2025-09-01 18:05:38 -0300

    primeiro commit

---
+++ src/main/java/com/guilhermesb/demo/GuilhermesbApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermesb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilhermesbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuilhermesbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermesb/demo/GuilhermesbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermesb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilhermesbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
