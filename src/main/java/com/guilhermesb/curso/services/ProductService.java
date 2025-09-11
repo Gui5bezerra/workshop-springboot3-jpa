2e052ebc44f86212657d79adf5ea45d713e30d1a
Gui5bezerra <guilhermesbezerra1@gmail.com>
2025-09-10 21:29:56 -0300

    Product

---
+++ src/main/java/com/guilhermesb/curso/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.guilhermesb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilhermesb.curso.entities.Product;
+import com.guilhermesb.curso.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional <Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
